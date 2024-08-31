0dee292de67e450989c3bc476488f1a4fec8d639
André Martins Brandão <andre53793016@aluno.ms.senac.br>
2024-08-31 17:25:36 -0400

    Update Diretorios.php

--- class/Diretorios.php
+++ class/Diretorios.php
@@ -54,7 +54,8 @@ class Diretorios {
             // Diretório não contém um repositório Git, clonar
             $command = "git clone $repoUrl " . escapeshellarg($repoDir);
         }
-    
+        echo "<pre>Comando: $command</pre>";
+        
         $result = $this->runCommand($command);
     
         if ($result === null || $result['return_code'] !== 0) {
